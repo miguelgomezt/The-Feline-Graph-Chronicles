db4ef67aca9c48c39e9178e3b5efc9e68f81e522
Miguel Gómez Tobón <miguel.gomez35@eia.edu.co>
2026-09-13 13:17:55 -0500

    Se agrega la clase sentinela

--- src/core/Sentinels.java
+++ src/core/Sentinels.java
@@ -1,4 +1,35 @@
 package core;
 
 public class Sentinels {
+    public static final long NO_ROUTE = Long.MAX_VALUE;
+
+    public static final long NO_ROUTE_INT = Integer.MIN_VALUE;
+
+    public static final long UNBOUNDED = Long.MAX_VALUE;
+
+    private Sentinels() {
+
+    }
+
+    public static boolean isNoRoute(long value){
+        return value == NO_ROUTE;
+    }
+
+    public static boolean isNoRoute(int value){
+        return value == NO_ROUTE_INT;
+    }
+
+    public static boolean isUnbounded(long value){
+        return value == UNBOUNDED;
+    }
+
+    public static long safeAdd(long a, long b){
+        if(isNoRoute(a) || isNoRoute(b)){
+            return NO_ROUTE;
+        }
+        if (isUnbounded(a) || isUnbounded(b)){
+            return UNBOUNDED;
+        }
+        return a+b;
+    }
 }
