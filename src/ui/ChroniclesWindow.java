484022b7893b5e233f854b2d9072b1ad350c6eb4
Miguel Gómez Tobón <miguel.gomez35@eia.edu.co>
2026-09-14 19:22:36 -0500

    Mejora el contraste de las pestanas de mision

--- src/ui/ChroniclesWindow.java
+++ src/ui/ChroniclesWindow.java
@@ -8,11 +8,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChroniclesWindow extends JFrame {
+
+    private final JTabbedPane pestanas = new JTabbedPane();
+    private final List<JLabel> etiquetas = new ArrayList<JLabel>();
 
     public ChroniclesWindow() {
         setTitle("The Feline Graph Chronicles - Universidad EIA");
@@ -28,18 +33,47 @@ public class ChroniclesWindow extends JFrame {
         List<Mission> misiones = new ArrayList<Mission>();
         misiones.add(new MinefieldMission());
 
-        JTabbedPane pestanas = new JTabbedPane();
         pestanas.setFont(Theme.NORMAL);
-        pestanas.setBackground(Theme.PANEL);
+        pestanas.setBackground(Theme.FONDO);
         pestanas.setForeground(Theme.TEXTO);
+        pestanas.setFocusable(false);
+        pestanas.setBorder(BorderFactory.createEmptyBorder(4, 8, 0, 8));
 
         for (int i = 0; i < misiones.size(); i++) {
             Mission mision = misiones.get(i);
             pestanas.addTab("Mision " + (i + 1), new MissionPanel(mision));
             pestanas.setToolTipTextAt(i, mision.getName());
+
+            JLabel etiqueta = new JLabel("Mision " + (i + 1));
+            etiqueta.setFont(Theme.NORMAL);
+            etiqueta.setOpaque(true);
+            etiqueta.setBorder(BorderFactory.createEmptyBorder(7, 18, 7, 18));
+            etiquetas.add(etiqueta);
+            pestanas.setTabComponentAt(i, etiqueta);
         }
 
+        pestanas.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                pintarPestanas();
+            }
+        });
+        pintarPestanas();
+
         add(pestanas, BorderLayout.CENTER);
+    }
+
+    private void pintarPestanas() {
+        int seleccionada = pestanas.getSelectedIndex();
+        for (int i = 0; i < etiquetas.size(); i++) {
+            JLabel etiqueta = etiquetas.get(i);
+            if (i == seleccionada) {
+                etiqueta.setBackground(Theme.HEROINAS);
+                etiqueta.setForeground(Theme.FONDO);
+            } else {
+                etiqueta.setBackground(Theme.PANEL);
+                etiqueta.setForeground(Theme.TEXTO_TENUE);
+            }
+        }
     }
 
     private JPanel construirEncabezado() {
